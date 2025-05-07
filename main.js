b1ec91eaf3734d41c3c09ce556ba41f2d7c06023
Bunyamin Ozturk <bunyamin.ozturk.tr@gmail.com>
2025-05-07 23:31:09 +0300

    colorful cells

--- main.js
+++ main.js
@@ -1,26 +1,13 @@
-let numOfCell = 50;
+let numOfCell = 20;
+document.querySelector("#buttonSetPixel").addEventListener("click", function (){
+  numOfCell = Number(prompt("How many squares?"));
 
-function makeSquares() {
-    for(let j=1; j<=numOfSquares; j++){
-        const container = document.createElement('div');
-        container.style.display = "flex";
-    
-        for(let i=1; i<=numOfSquares; i++){
-            const square = document.createElement('div');
-            square.style.width = '30px';
-            square.style.height = '30px';
-            square.style.backgroundColor = '#e9e9e9';
-            square.style.margin = '1px';
-            square.addEventListener('mouseover', function () {
-            square.style.backgroundColor = "red";
-      });
-      container.appendChild(square);
-    }
-    document.body.appendChild(container);
-    }
-    
-}
-// makeSquares();
+makeCell();
+
+});
+
+
+function makeCell(){
 
 // Grid container
 const container = document.createElement('div');
@@ -28,7 +15,7 @@ const container = document.createElement('div');
 // Container styles
 container.style.display = "grid";
 container.style.width = "50%";     
-container.style. height = "535px";     
+container.style. height = "510px";     
 container.style. gridTemplateColumns = `repeat(${numOfCell}, 1fr)`;  
 container.style.gridTemplateRows = `repeat(${numOfCell}, 1fr)`; 
 container.style. gap = "1px";     
@@ -36,17 +23,15 @@ container.style. margin = "auto";
 container.style.marginTop = "5vmin";       
 container.style.border = "2px solid #333";
 
-
+// Cells
 for (let i=0; i<numOfCell*numOfCell; i++) {
   const cell = document.createElement('div'); 
-
   cell.style.backgroundColor = "#ddd";
-  cell.style.width = "90%";
+  cell.style.width = "100%";
   cell.style.height = "100%";
   cell.addEventListener("mouseover", function () {
   cell.style.backgroundColor = randomColor();
   });
-
   container.appendChild(cell);
 }
 
@@ -55,11 +40,11 @@ document.body.appendChild(container);
 document.body.style.margin = "0";                
 document.body.style.backgroundColor = "#f0f0f0"; 
 
-
 function randomColor() {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    return `rgb(${r},${g},${b})`;
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  console.log(`rgb(${r},${g},${b})`);
+  return `rgb(${r},${g},${b})`;
   }
-
+}
